51f3c1afe5b70715a62bb46401105376b99899e5
Callum Owen <callumowen6@googlemail.com>
2020-05-26 22:08:37 +0100

    53_PB level design start

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+	[SerializeField] float rcsThrust = 100f;
+	[SerializeField] float mainThrust = 100f;
 
 	Rigidbody rigidBody;
 	AudioSource audioSource;
@@ -23,17 +25,35 @@ public class Rocket : MonoBehaviour
 		Thrust();
 	}
 
+	void OnCollisionEnter(Collision collision)
+	{
+		switch (collision.gameObject.tag)
+		{
+			case "Friendly":
+				print("OK");	//todo remove
+				break;
+			case "Fuel":
+				print("Fuel");	//todo remove
+				break;
+			default:
+				print("dead");	//todo kill player
+				break;
+		}
+	}
+
 	private void Rotate()
 	{
-		rigidBody.freezeRotation = true;	//take manual control of rotation
+		rigidBody.freezeRotation = true;    //take manual control of rotation
+
+		float rotationThisFrame = rcsThrust * Time.deltaTime;
 
 		if (Input.GetKey(KeyCode.A))
 		{
-			transform.Rotate(Vector3.forward);
+			transform.Rotate(Vector3.forward * rotationThisFrame);
 		}
 		else if (Input.GetKey(KeyCode.D))
 		{
-			transform.Rotate(-Vector3.forward);
+			transform.Rotate(-Vector3.forward * rotationThisFrame);
 		}
 
 		rigidBody.freezeRotation = false; //resume physics control of rotation
@@ -41,9 +61,11 @@ public class Rocket : MonoBehaviour
 
 	private void Thrust()
 	{
+		float thrustThisFrame = mainThrust * Time.deltaTime;
+
 		if (Input.GetKey(KeyCode.Space))    //can thrust while rotating
 		{
-			rigidBody.AddRelativeForce(Vector3.up);
+			rigidBody.AddRelativeForce(Vector3.up * thrustThisFrame);
 			if (!audioSource.isPlaying)		//so it doesn't layer
 			{
 				audioSource.Play();
