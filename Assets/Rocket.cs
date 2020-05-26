d04e22abd17df685ca614a429fbeb2b808414330
Callum Owen <callumowen6@googlemail.com>
2020-05-26 20:19:18 +0100

    Adding audio on space press

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,12 +6,14 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
 
-	Rigidbody rigidBody; 
+	Rigidbody rigidBody;
+	AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
 		rigidBody = GetComponent<Rigidbody>();
+		audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -25,6 +27,13 @@ public class Rocket : MonoBehaviour
 		if (Input.GetKey(KeyCode.Space))	//can thrust while rotating
 		{
 			rigidBody.AddRelativeForce(Vector3.up);
+			if (!audioSource.isPlaying)
+			{
+				audioSource.Play();
+			}
+		} else
+		{
+			audioSource.Stop();
 		}
 
 		if (Input.GetKey(KeyCode.A))
